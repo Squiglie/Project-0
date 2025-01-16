903dbe7cf4b8d7df83988698f8f53aff7e86e858
Mariana  Mora <marianamora@MacBook-Pro-6.local>
2025-01-16 10:31:03 -0800

    half orange half pink

--- sketch.js
+++ sketch.js
@@ -1,7 +1,3 @@
-let xLoc = 50;
-let xSpeed = 1;
-
-
 function setup() {
   createCanvas(600, 600);
 
@@ -11,26 +7,20 @@ function setup() {
 function draw() {
   background(200,0,155);
   
-  
-
-
-  xLoc = xLoc + xSpeed;
-  if(xLoc > width - 50 || xLoc < 50){
-    xSpeed = xSpeed *-1;}
-  stroke(255, 100, 10);
-  strokeWeight(4);
-  fill(200, 0, 155);
-  ellipse(xLoc, xLoc, 100, 100);
-
-
+  noStroke();
   rotate(QUARTER_PI);
   fill(255, 100, 10);
-  rectMode(CENTER);
-  rect(300, 300, 300, 580);
+  //rectMode(CENTER);
+  rect(0, 0, 1000, 1000);
+
+
+  // rect(600, 200, 20, 20);
+
+  
  
 
   
-
+  
 
   
   
