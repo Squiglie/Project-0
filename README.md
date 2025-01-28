c83fd6b45e90f29f1792cc9b48646c482ac87a96
Mariana  Mora <marianamora@MacBook-Pro-9.local>
2025-01-28 11:38:36 -0800

    final push

---
+++ README.md
@@ -0,0 +1,9 @@
+#steep-and-fast
+##view a live version at https://squiglie.github.io/Project-0/
+
+git status
+git add .
+git pull
+
+git commit -am'comment of changes'
+git push
--- sketch.js
+++ sketch.js
@@ -4,27 +4,33 @@
 function setup(){
   createCanvas(600, 600); 
   noLoop()
+  angleMode(DEGREES);
 }
 
 function draw(){
   background(200,0,155);
   
   fill(255, 100, 0);
- 
+  
+  // rectMode(RADIUS);
+
+  rotate(90);
+  translate(0, -600);
+
   strokeWeight(4);
   stroke(255,0,0);  
   let sz = 20
-  for(let y = 0; y < height; y = y+sz){
-     for(let x = 0; x < width ; x = x+sz){
+  for(let y = 10; y < height; y = y+sz){
+     for(let x = 10; x < width ; x = x+sz){
 
        //move diagonally down the 'index' of the for loop, 
        // start at the width, but we have to minus a sz 
        // for each step of the loop to get back to the 
        // "middle" of the diagonal
-       if(y+x < width+-sz*2){
+       if(y+x < width+-sz){
          
          //this is just  to prove that it's not one long line!
-         if(random(100)> 20){
+         if(random(60)> 20){
             line(y,sz+ x, sz+y, x);
          }
          
